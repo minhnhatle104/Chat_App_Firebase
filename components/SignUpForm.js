6ff56b79fd735ce6d524eaeedc9212f54faf60ef
minhnhatle104 <lnminh104@gmail.com>
2023-03-12 10:47:57 +0700

    validate form SignIn + SignUp

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,36 +1,75 @@
-import React from 'react';
+import React, { useCallback, useReducer } from 'react';
 import Input from '../components/Input';
 import SubmitButton from '../components/SubmitButton';
 import { Feather, FontAwesome } from '@expo/vector-icons';
 
+import { validateInput } from '../utils/actions/formActions';
+import { reducer } from '../utils/reducers/formReducer';
+
+const initialState = {
+    inputValidities: {
+        firstName: false,
+        lastName: false,
+        email: false,
+        password: false,
+    },
+    formIsValid: false
+}
+
 const SignUpForm = props => {
+
+    const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
+    const inputChangedHandler = useCallback((inputId, inputValue) => {
+        const result = validateInput(inputId, inputValue);
+        dispatchFormState({ inputId, validationResult: result })
+    }, [dispatchFormState]);
 
     return (
             <>
                 <Input
+                    id="firstName"
                     label="First name"
                     icon="user-o"
-                    iconPack={FontAwesome} />
+                    iconPack={FontAwesome}
+                    onInputChanged={inputChangedHandler}
+                    autoCapitalize="none"
+                    errorText={formState.inputValidities["firstName"]} />
 
                 <Input
+                    id="lastName"
                     label="Last name"
                     icon="user-o"
-                    iconPack={FontAwesome} />
+                    iconPack={FontAwesome}
+                    onInputChanged={inputChangedHandler}
+                    autoCapitalize="none"
+                    errorText={formState.inputValidities["lastName"]} />
 
                 <Input
+                    id="email"
                     label="Email"
                     icon="mail"
-                    iconPack={Feather} />
+                    iconPack={Feather}
+                    onInputChanged={inputChangedHandler}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    errorText={formState.inputValidities["email"]} />
 
                 <Input
+                    id="password"
                     label="Password"
                     icon="lock"
-                    iconPack={Feather} />
+                    autoCapitalize="none"
+                    secureTextEntry
+                    iconPack={Feather}
+                    onInputChanged={inputChangedHandler}
+                    errorText={formState.inputValidities["password"]} />
                 
                 <SubmitButton
                     title="Sign up"
                     onPress={() => console.log("Button pressed")}
-                    style={{ marginTop: 20 }}/>
+                    style={{ marginTop: 20 }}
+                    disabled={!formState.formIsValid}/>
             </>
     )
 };
