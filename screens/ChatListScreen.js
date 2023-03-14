0f80588b089c1ca99b96e6ca6cc7e77deea98d59
minhnhatle104 <lnminh104@gmail.com>
2023-03-14 10:36:16 +0700

    Create a new chat

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const ChatListScreen = props => {
+
+    const selectedUser = props.route?.params?.selectedUserId;
+
+    const userData = useSelector(state => state.auth.userData);
 
     useEffect(() => {
         props.navigation.setOptions({
@@ -17,6 +22,22 @@ const ChatListScreen = props => {
             }
         })
     }, []);
+
+    useEffect(() => {
+
+        if (!selectedUser) {
+            return;
+        }
+        
+        const chatUsers = [selectedUser, userData.userId];
+
+        const navigationProps = {
+            newChatData: { users: chatUsers }
+        }
+
+        props.navigation.navigate("ChatScreen", navigationProps);
+
+    }, [props.route?.params])
     
     return <View style={styles.container}>
         <Text>Chat list screen</Text>
--- screens/NewChatScreen.js
+++ screens/NewChatScreen.js
@@ -8,13 +8,19 @@ import colors from '../constants/colors';
 import commonStyles from '../constants/commonStyles';
 import { searchUsers } from '../utils/actions/userActions';
 import DataItem from '../components/DataItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { setStoredUsers } from '../store/userSlice';
 
 const NewChatScreen = props => {
+
+    const dispatch = useDispatch();
 
     const [isLoading, setIsLoading] = useState(false);
     const [users, setUsers] = useState();
     const [noResultsFound, setNoResultsFound] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
+
+    const userData = useSelector(state => state.auth.userData);
 
     useEffect(() => {
         props.navigation.setOptions({
@@ -40,6 +46,7 @@ const NewChatScreen = props => {
             setIsLoading(true);
 
             const usersResult = await searchUsers(searchTerm);
+            delete usersResult[userData.userId];
             setUsers(usersResult);
 
             if (Object.keys(usersResult).length === 0) {
@@ -47,6 +54,8 @@ const NewChatScreen = props => {
             }
             else {
                 setNoResultsFound(false);
+
+                dispatch(setStoredUsers({ newUsers: usersResult }))
             }
 
             setIsLoading(false);
@@ -54,6 +63,12 @@ const NewChatScreen = props => {
 
         return () => clearTimeout(delaySearch);
     }, [searchTerm]);
+
+    const userPressed = userId => {
+        props.navigation.navigate("ChatList", {
+            selectedUserId: userId
+        })
+    }
     
     return <PageContainer>
         <View style={styles.searchContainer}>
@@ -85,6 +100,7 @@ const NewChatScreen = props => {
                                 title={`${userData.firstName} ${userData.lastName}`}
                                 subTitle={userData.about}
                                 image={userData.profilePicture}
+                                onPress={() => userPressed(userId)}
                             />
                 }}
             />
